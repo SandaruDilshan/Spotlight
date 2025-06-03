b34016e3add14a35c9b8924333d6ce8be7e1974c
Sandaru D Chandrasiri <130263550+SandaruDilshan@users.noreply.github.com>
2025-06-03 17:14:10 +0530

    initial commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,50 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Tabs } from 'expo-router'
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { COLORS } from '@/constants/theme';
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+export default function TabsLayouts() {
+    return (
+        <Tabs screenOptions={{
+            headerShown: false,
+            tabBarShowLabel: false,
+            tabBarActiveTintColor: COLORS.primary,
+            tabBarInactiveTintColor: COLORS.grey,
+            tabBarStyle: {
+                backgroundColor: 'black',
+                borderTopWidth: 0,
+                position: 'absolute',
+                height: 40,
+                paddingBottom:8,
+            }
+        }}>
+            <Tabs.Screen name='index'
+                options={{                                                                                
+                    tabBarIcon: ({ size, color }) => <Ionicons name='home' size={size} color={color} /> //Tab icon option defined as a function that returns an Ionicons component dynamically
+                }}                                                                                       //"dynamically" means based on the current state or input at runtime — not hardcoded or fixed in advance.
+            />
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+            <Tabs.Screen name='bookmarks'
+                options={{
+                    tabBarIcon: ({ size, color }) => <Ionicons name='bookmark' size={size} color={color} />
+                }}
+            />
+            <Tabs.Screen name='create'
+                options={{
+                    tabBarIcon: ({ size, color }) => <Ionicons name='add-circle' size={size} color={COLORS.primary} />
+                }}
+            />
+            <Tabs.Screen name='notifications'
+                options={{
+                    tabBarIcon: ({ size, color }) => <Ionicons name='notifications' size={size} color={color} />
+                }}
+            />
+            <Tabs.Screen name='profile'
+                options={{
+                    tabBarIcon: ({ size, color }) => <Ionicons name='person-circle' size={size} color={color} />
+                }}
+            />
+        </Tabs>
+    )
 }
